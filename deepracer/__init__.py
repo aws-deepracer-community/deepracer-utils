549b27b48a9bf06fa75912afc87a5d0d145f2425
Tomasz Ptak <tomasz.ptak@gmail.com>
2020-12-13 16:25:08 +0000

    Add deepracer client definition to boto3

--- deepracer/__init__.py
+++ deepracer/__init__.py
@@ -1,5 +1,8 @@
-from . import logs, tracks
+import os
+from . import logs, tracks, boto3
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+DEEPRACER_UTILS_ROOT = os.path.dirname(os.path.abspath(__file__))
