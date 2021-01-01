e3eddb73f4e639c1bd1021c89cc37cb41188e50d
Tomasz Ptak <tomasz.ptak@gmail.com>
2021-01-01 21:00:38 +0000

    fix pycodestyle

--- deepracer/__init__.py
+++ deepracer/__init__.py
@@ -1,9 +1,9 @@
 import os
 
-DEEPRACER_UTILS_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 from . import logs, tracks, boto3_enhancer
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+DEEPRACER_UTILS_ROOT = os.path.dirname(os.path.abspath(__file__))
