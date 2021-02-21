17c5960812af1c4b985747adb58d73cf575b43e2
Lars Lorentz Ludvigsen <59617571+larsll@users.noreply.github.com>
2021-02-21 17:25:03 +0100

    Fixing Metrics loading (#16)
    * Adding profile to module
    
    * Loading bugfix
    
    * Renaming Metrics

--- deepracer/logs/__init__.py
+++ deepracer/logs/__init__.py
@@ -1,4 +1,4 @@
 from .log_utils import ActionBreakdownUtils, AnalysisUtils, EvaluationUtils, NewRewardUtils, \
     PlottingUtils, SimulationLogsIO
-from .load_metrics import TrainingMetrics
+from .metrics import TrainingMetrics
 from .log import DeepRacerLog
