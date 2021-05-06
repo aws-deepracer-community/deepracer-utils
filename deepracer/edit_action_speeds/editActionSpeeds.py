ec67be61e82cef197e1767a494172cff1b7e4ba2
n0237580 <darren.broderick@liberty-it.co.uk>
2021-05-06 19:11:12 +0100

    Filename as input param and some renaming

--- deepracer/edit_action_speeds/editActionSpeeds.py
+++ deepracer/edit_action_speeds/editActionSpeeds.py
@@ -2,18 +2,17 @@ import json
 import os
 import uuid
 
-filename = 'action_space.json'
 
 # README
 # Paste in your full action space into the file "action_space.json", it can handle index values if you have them already
 # Run this script with desired parameters on the last line"
 
 
-def edit_action_speeds(edit_value, change_left_steering, change_straight, change_right_steering, add_indexer):
-    with open(filename) as action_space:
-        data = json.load(action_space)
+def edit_action_speeds(filename, edit_value, change_left_steering, change_straight, change_right_steering, add_indexer):
+    with open(filename) as action_space_file:
+        action_space_data = json.load(action_space_file)
         index_counter = 0
-        for action in data['action_space']:
+        for action in action_space_data['action_space']:
             if add_indexer:
                 action['index'] = index_counter
                 index_counter += 1
@@ -28,12 +27,13 @@ def edit_action_speeds(edit_value, change_left_steering, change_straight, change
                     action_update(action, edit_value)
 
     # Create randomly named temporary file to avoid interference with other thread/asynchronous request
-    tempfile = os.path.join(os.path.dirname(filename), str(uuid.uuid4()))
-    with open(tempfile, 'w') as temp:
-        json.dump(data, temp, indent=4)
+    tmpfile = os.path.join(os.path.dirname(filename), str(uuid.uuid4()))
+
+    with open(tmpfile, 'w') as temp:
+        json.dump(action_space_data, temp, indent=4)
 
     # Rename temporary file replacing old file
-    os.rename(tempfile, filename)
+    os.rename(tmpfile, filename)
 
 
 def action_update(action, edit_value):
@@ -48,14 +48,14 @@ def action_update(action, edit_value):
         action['speed'] = round((action['speed'] + edit_value), 2)
 
 
-# 1st Parameter either a positive or negative value for editing all speeds the same value
-# 2nd/3rd/4th Parameter boolean for changing left(positive), straight or right steering angles
-# 5th Parameter is for adding index values
+# 1st is filename of action space (metadata.json)
+# 2nd Parameter either a positive or negative value for editing all speeds the same value
+# 3rd/4th/5th Parameter boolean for changing left(positive), straight or right steering angles
+# 6th Parameter is for adding index values
 
 
 # EXAMPLE 1 - Add Speed of 0.5 to all actions, with updating index values
-edit_action_speeds(0.5, True, True, True, True)
-
+edit_action_speeds('action_space.json', 0.5, True, True, True, True)
 
 # EXAMPLE 2 - Minus Speed of 0.1 to all actions, with no change to index values
-# edit_action_speeds(-0.1, True, True, True, False)
+# edit_action_speeds('action_space.json', -0.1, True, True, True, False)
